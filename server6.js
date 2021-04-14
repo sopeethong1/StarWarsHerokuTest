ae2070a5ba60178edaaf48751e13acad9a726764
Sopee Thong <tsopee@gmail.com>
2021-04-14 12:24:06 -0400

    heroku test

--- server6.js
+++ server6.js
@@ -7,7 +7,7 @@ const path = require('path');
 // Sets up the Express App
 //9-14 just doc to set up 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
